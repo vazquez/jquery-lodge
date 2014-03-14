58aeab58a34f022549dd25ac5468c710627fa482
vazquez <2ricardo.vazquez@gmail.com>
2014-03-14 09:56:10 -0700

    Organizing files

---
+++ 1. Select Do/js/main.js
@@ -0,0 +1,16 @@
+// // JS is a Select Do library.
+
+// // Select!
+// $("h1 span")
+//   // Do!
+// .css({
+//   "color": "red",
+//   "font-family": "Georgia"
+// });
+// // Notice how we are using an object since we need to change more than 1 CSS property.
+
+// // Select!
+// $("div")
+//   // Do!
+//   .addClass("callout");
+
