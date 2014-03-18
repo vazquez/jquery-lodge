13e36f87a210ed235ab6d0840d6945f5f5e3aa1b
vazquez <2ricardo.vazquez@gmail.com>
2014-03-17 17:47:38 -0700

    Adding window load function and description

--- 2. DOM Ready/js/main.js
+++ 2. DOM Ready/js/main.js
@@ -13,3 +13,10 @@ $(document).ready(function() {
 
 // It won't wait for a super large image to load. But it will read the DOM and know that an image is gonna go there, and thus be able to assign the JS handler appropriately
 
+// Putting code inside a function like that endure it won't run until the document is ready.
+
+// If you do need to wait for resources to be done (e.g. you need to measure an image), you can wait for that like this:
+
+$(window).load(function() {
+  // Code goes here
+});
